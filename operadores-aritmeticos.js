3cff34884be9699cd205ac486035fe95fee8e99d
JonathasSampaio <jonathas.sampaio@gmail.com>
2022-12-08 19:50:56 -0300

    Lógica.js

---
+++ operadores-aritmeticos.js
@@ -0,0 +1,34 @@
+/* OPERADORES aritimetiméticos
+
+ +   --> Adição
+ -   --> Subitração
+ *   --> Multiplicação
+ /   --> Divisão
+ %   --> Resto
+ ++  --> Incremento
+--   --> Decremento
+**   --> Exponencial
+*/
+
+
+console.log (7 / 3)              // imprime 2.3.3.3.3.3
+ console.log (7 % 3)           // imprime 1
+
+
+let myNumber = 10
+console.log (myNumber + 10)    // imprime 20 
+
+
+ let myNumber = 10
+myNumber++              incrementa 1  -- decrementa
+myNunber++
+console.log (myNumber)        // imprime 11
+
+
+ let myNumber = 10
+console.log (++myNumber)        //  imprime 11
+
+
+
+
+console.log(2 ** 2)          // imprime 4  
